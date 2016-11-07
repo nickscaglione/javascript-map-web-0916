5adcb4c29693b4c00c948abb9e620ea94a09aae6
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-11-07 08:58:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element, i, collection));
+  }
+
+  return result;
+}
